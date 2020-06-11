6161c86a1aba99ff71bc5c3301d6be6832bddd97
Aparna M <aparnamanoj98@gmail.com>
2020-06-11 16:41:36 +0530

    removed the election factory contract

--- contracts/ElectionNew.sol
+++ contracts/ElectionNew.sol
@@ -3,182 +3,9 @@ pragma solidity ^0.5.16;
 
 pragma experimental ABIEncoderV2; // needed to be able to pass string arrays and structs into functions
 
-/// @dev This contract spawns election contracts that can be used for voting
-contract ElectionFactory {
-
-    address public registrationAuthorityContractAdd; //stores the contract address of a deployed registration authority
-    address public registrationAuthority; // stores the accout of the registration authority
-    address[] public deployedElections; // keeps a list of all deployed elections
-
-    /// @dev initializes the contract and sets the registration authority to be the deployer of the contract
-    constructor(address _registrationAuthorityContAdd) public {
-        registrationAuthorityContractAdd= _registrationAuthorityContAdd;
-        registrationAuthority = msg.sender;
-    }
-
-    /// @dev only the registration authority is allowed functions marked with this
-    /// @notice functions with this modifier can only be used by the registration authority
-    modifier restricted() {
-        require(msg.sender == registrationAuthority, "only the registration authority is allowed to use this function");
-        _;
-    }
-
-    /// @dev use this to deploy new Election contracts and reset the temporary options lists afterwards
-    /// @param _title specifies the name of the election (e.g. national elections)
-    /// @param _description specifies the description of the election
-    /// @param _startTime specifies the beginning of the election (since Unix Epoch in seconds)
-    /// @param _timeLimit specifies a time limit until when the election is open (since Unix Epoch in seconds)
-    function createElection(
-        string memory _title,
-        string memory _description,
-        uint _startTime,
-        uint _timeLimit,
-        string memory _encryptionKey)
-        public restricted {
-        deployedElections.push(
-            address(
-                new Election(
-                    registrationAuthority,
-                    registrationAuthorityContractAdd,
-                    _title,
-                    _description,
-                    _startTime,
-                    _timeLimit,
-                    _encryptionKey
-                )
-            )
-        );
-    }
-
-    /// @dev use this to return a list of addresses of all deployed Election contracts
-    /// @return a list of addresses of all deployed Election contracts
-    function getDeployedElections() public view returns(address[] memory) {
-        return deployedElections;
-    }
-
-}
-
-/// @dev This is the actual election contract where users can vote
-/// @dev Security by design: secret sharing, allows voters one time voting
-contract Election {
-    struct Ballot {
-        string name;
-        string party;
-    }
-
-/*    struct Vote {
-        uint listPointer; // index in the list of addresses that voted
-        string encryptedVote; // homomorphically encrypted 0 or 1 for each option. 1 being a vote. Max 1 per voter.
-    }*/
-
-    address public registrationAuthority;
-    address public  registrationAuthorityContractAdd;
-    string public title;
-    string public description;
-    uint public startTime;
-    uint public timeLimit;
-    uint public timeNow;
-    Ballot[] public ballotList;
-    string public encryptionKey;
-    uint[] public publishedResult;
-
-    mapping(address => bool) private votersCheckList; // records that the voter has voted
-    string[] private encryptedVoteList; // keeps a list of all encrypted votes
-
-    /// @dev initializes the contract with all required parameters and sets the manager of the contract
-    constructor(
-        address _registrationAuthority,
-        address _registrationAuthorityContractAdd,
-        string memory _title,
-        string memory _description,
-        uint _startTime,
-        uint _timeLimit,
-        string memory _encryptionKey
-    ) public {
-        registrationAuthority=_registrationAuthority;
-        registrationAuthorityContractAdd = _registrationAuthorityContractAdd;
-        title = _title;
-        description = _description;
-        startTime = _startTime;
-        timeLimit = _timeLimit;
-        encryptionKey = _encryptionKey;
-    }
-
-    /// @dev only the registration authority is allowed functions marked with this
-    /// @notice functions with this modifier can only be used by the registration authority
-    modifier restricted() {
-        require(msg.sender == registrationAuthority, "only the registration authority is allowed to use this function");
-        _;
-    }
-
-    /// @dev functions marked with this can be called before the specified start time
-    modifier beforeElection() {
-        require(now < startTime, "only allowed before election");
-        _;
-    }
-
-    /// @dev functions marked with this can be called during the specified time frame
-    modifier duringElection() {
-        require(now > startTime && now < timeLimit, "only allowed during election");
-        _;
-    }
-
-    /// @dev functions marked with this can be called after the specified end time
-    modifier afterElection() {
-        require(now > timeLimit, "only allowed after election");
-        _;
-    }
-
-    /// @dev add an option to the ballot before the election starts
-    function addCandidate(string calldata _name, string calldata _party) external restricted beforeElection {
-        ballotList.push(Ballot({ name: _name, party: _party }));
-    }
-
-    /// @dev get all available options on the ballot
-    function getBallot() external view returns(Ballot[] memory x) {
-        return ballotList;
-    }
-
-    /// @dev get the list of encrypted votes of a voter, only allowed after the election is over
-    function getencryptedVoteList() external view restricted afterElection returns(string[] memory success) {
-        return encryptedVoteList;
-    }
-    /// @dev publish the decrypted version of the sum of all votes for each candidate
-    function publishResults(uint[] calldata results) external restricted afterElection returns(bool success) {
-        publishedResult = results;
-        return true;
-    }
-
-    /// @dev returns the list of final votes for each candidate
-    function getResults() external view afterElection returns(uint[] memory results) {
-        return publishedResult;
-    }
-
-    /// @dev this is used to cast a vote. the vote is homomorphically encrypted
-    /// @dev allows users to vote multiple times, invalidating the previous vote
-    function vote(string calldata _encryptedVote) external duringElection returns(bool success) {
-        require(isRegisteredVoter(msg.sender), "message sender is not a registered voter");
-        require(!votersCheckList[msg.sender],"only one vote possible");
-        encryptedVoteList.push(_encryptedVote);
-        votersCheckList[msg.sender] = true;
-
-        return true;
-    }
-
-    /// @dev find out whether a voter has submitted their vote
-    function hasVoted(address _address) public view returns(bool) {
-        return votersCheckList[_address];
-    }
-
-    /// @dev check the registration authority whether the address is registered as a valid voter
-    function isRegisteredVoter(address _address) private view returns(bool) {        
-        RegistrationAuthority ra = RegistrationAuthority(registrationAuthorityContractAdd);
-        return ra.isVoter(_address);
-    }
-}
-
 /// @dev use this to register and unregister voters
 contract RegistrationAuthority {
+    
     struct Voter {
         uint listPointer;
         bool isVoter;
@@ -189,23 +16,26 @@ contract RegistrationAuthority {
         string personId;
     }
 
-    address public manager;
+    address public registrationAuthority;
 
     mapping(address => Voter) private voters;
     address[] private votersReferenceList;
 
-    /// @dev initializes the contract and sets the contract manager to be the deployer of the contract
+    address[] public deployedElections; // keeps a list of all deployed elections
+
+    /// @dev initializes the contract and sets the contract registration authority to be the deployer of the contract
     constructor() public {
-        manager = msg.sender;
+        registrationAuthority = msg.sender;
     }
 
-    /// @dev only the factory manager is allowed functions marked with this
+    /// @dev only the registration authority is allowed functions marked with this
     /// @notice functions with this modifier can only be used by the administrator
     modifier restricted() {
-        require(msg.sender == manager, "only the contract manager is allowed to use this function");
+        require(msg.sender == registrationAuthority, "only the registration authority is allowed to use this function");
         _;
     }
 
+    /************Voter related function*******************/
     /// @dev use this to register or update a voter
     function registerOrUpdateVoter(
         address _voter,
@@ -259,4 +89,156 @@ contract RegistrationAuthority {
     function getVoterDetails(address _voter) public view returns(Voter memory) {
         return voters[_voter];
     }
+
+
+    /************Election related function*******************/
+    /// @dev use this to deploy new Election contracts and reset the temporary options lists afterwards
+    /// @param _title specifies the name of the election (e.g. national elections)
+    /// @param _description specifies the description of the election
+    /// @param _startTime specifies the beginning of the election (since Unix Epoch in seconds)
+    /// @param _endTime specifies a time limit until when the election is open (since Unix Epoch in seconds)
+    function createElection(
+        string memory _title,
+        string memory _description,
+        uint _startTime,
+        uint _endTime,
+        string memory _encryptionKey)
+        public restricted {
+        deployedElections.push(
+            address(
+                new Election(
+                    registrationAuthority,
+                    _title,
+                    _description,
+                    _startTime,
+                    _endTime,
+                    _encryptionKey
+                )
+            )
+        );
+    }
+
+    /// @dev use this to return a list of addresses of all deployed Election contracts
+    /// @return a list of addresses of all deployed Election contracts
+    function getDeployedElections() public view returns(address[] memory) {
+        return deployedElections;
+    }
 }
+
+/// @dev This is the actual election contract where users can vote
+/// @dev Security by design: secret sharing, allows voters one time voting
+contract Election {
+    struct Ballot {
+        string name;
+        string party;
+    }
+
+/*    struct Vote {
+        uint listPointer; // index in the list of addresses that voted
+        string encryptedVote; // homomorphically encrypted 0 or 1 for each option. 1 being a vote. Max 1 per voter.
+    }*/
+
+    address public registrationAuthority;
+    address public  registrationAuthorityContractAdd;
+    string public title;
+    string public description;
+    uint public startTime;
+    uint public endTime;
+    Ballot[] public ballotList;
+    string public encryptionKey;
+    uint[] public publishedResult;
+
+    mapping(address => bool) private votersCheckList; // records that the voter has voted
+    string[] private encryptedVoteList; // keeps a list of all encrypted votes
+
+    /// @dev initializes the contract with all required parameters
+    constructor(
+        address _registrationAuthority,
+        string memory _title,
+        string memory _description,
+        uint _startTime,
+        uint _endTime,
+        string memory _encryptionKey
+    ) public {
+        registrationAuthority=_registrationAuthority;
+        registrationAuthorityContractAdd = msg.sender;
+        title = _title;
+        description = _description;
+        startTime = _startTime;
+        endTime = _endTime;
+        encryptionKey = _encryptionKey;
+    }
+
+    /// @dev only the registration authority is allowed functions marked with this
+    /// @notice functions with this modifier can only be used by the registration authority
+    modifier restricted() {
+        require(msg.sender == registrationAuthority, "only the registration authority is allowed to use this function");
+        _;
+    }
+
+    /// @dev functions marked with this can be called before the specified start time
+    modifier beforeElection() {
+        require(now < startTime, "only allowed before election");
+        _;
+    }
+
+    /// @dev functions marked with this can be called during the specified time frame
+    modifier duringElection() {
+        require(now > startTime && now < endTime, "only allowed during election");
+        _;
+    }
+
+    /// @dev functions marked with this can be called after the specified end time
+    modifier afterElection() {
+        require(now > endTime, "only allowed after election");
+        _;
+    }
+
+    /// @dev add an option to the ballot before the election starts
+    function addCandidate(string calldata _name, string calldata _party) external restricted beforeElection {
+        ballotList.push(Ballot({ name: _name, party: _party }));
+    }
+
+    /// @dev get all available options on the ballot
+    function getBallot() external view returns(Ballot[] memory x) {
+        return ballotList;
+    }
+
+    /// @dev get the list of encrypted votes of a voter, only allowed after the election is over
+    function getencryptedVoteList() external view restricted afterElection returns(string[] memory success) {
+        return encryptedVoteList;
+    }
+    /// @dev publish the decrypted version of the sum of all votes for each candidate
+    function publishResults(uint[] calldata results) external restricted afterElection returns(bool success) {
+        publishedResult = results;
+        return true;
+    }
+
+    /// @dev returns the list of final votes for each candidate
+    function getResults() external view afterElection returns(uint[] memory results) {
+        return publishedResult;
+    }
+
+    /// @dev this is used to cast a vote. the vote is homomorphically encrypted
+    /// @dev allows users to vote multiple times, invalidating the previous vote
+    function vote(string calldata _encryptedVote) external duringElection returns(bool success) {
+        require(isRegisteredVoter(msg.sender), "message sender is not a registered voter");
+        require(!votersCheckList[msg.sender],"only one vote possible");
+        encryptedVoteList.push(_encryptedVote);
+        votersCheckList[msg.sender] = true;
+
+        return true;
+    }
+
+    /// @dev find out whether a voter has submitted their vote
+    function hasVoted(address _address) public view returns(bool) {
+        return votersCheckList[_address];
+    }
+
+    /// @dev check the registration authority whether the address is registered as a valid voter
+    function isRegisteredVoter(address _address) private view returns(bool) {        
+        RegistrationAuthority ra = RegistrationAuthority(registrationAuthorityContractAdd);
+        return ra.isVoter(_address);
+    }
+}
+
